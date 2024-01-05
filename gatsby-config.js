1ddde123397ebad61f4f246e053e597eda3a0e94
Sethupathi <mysethu888@gmail.com>
2024-01-05 12:16:59 +0530

    newly added gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    siteMetadata: {
+      title: 'Sethu',
+      description: 'Projects for web development',
+      // Add other site metadata as needed
+    },
+    plugins: [
+      'gatsby-plugin-react-helmet',
+      // Add other plugins here
+    ],
+  };
